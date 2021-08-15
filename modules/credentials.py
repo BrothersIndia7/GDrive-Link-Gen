c3ef5d43974bc28855a5793a6dfc40c64fbe2fb3
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2021-08-15 19:16:47 +0530

    Update credentials.py

--- modules/credentials.py
+++ modules/credentials.py
@@ -1,6 +1,6 @@
 class Creds(object):
-    TOKEN = "bot token here"
-    CLIENT_ID = "client id from google cloud console"
-    CLIENT_SECRET = "client secret from google cloud console"
-    PATH_ID = "folder id of the gdrive folder in which the bot will upload its files"
-    USER_ID = "your telegram chat id"
+    TOKEN = "1746903731:AAFv2Ahh_pkyW9vJ4V7sUqMy2WTL26voRy8"
+    CLIENT_ID = "825923466109-6svkf1f5sr7kueqngd3j56l451753tb8.apps.googleusercontent.com"
+    CLIENT_SECRET = "LjOO-LUXUVCBlqA9IEbQUccX"
+    PATH_ID = "0AIprDZl1QwrJUk9PVA"
+    USER_ID = "1002182448"
