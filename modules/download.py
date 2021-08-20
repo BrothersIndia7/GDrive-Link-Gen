e3e231fd80d4f280d8c87eccf82b1570d3a3faae
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2021-08-20 15:17:22 +0530

    Update download.py

--- modules/download.py
+++ modules/download.py
@@ -35,12 +35,18 @@ def download(url, filename):
                 filename = re.sub('[(){}<>-]', '', name)
                 filename = re.sub(r'\[\[(?:[^|\]]*\|)?([^\]]*)]]', r'\1', filename)
 		downloader = Downloader(url, filename, 5)
-		downloader.start()
-		downloader.subscribe(callback, callback_threshold)
-		downloader.wait_for_finish()"""
-		if filename:
+                downloader.start()
+                downloader.subscribe(callback, callback_threshold)
+                downloader.wait_for_finish()"""
+	        if filename:
 			cmd_output = subprocess.check_output("wget -O '{}' '{}'".format(filename, url), stderr=subprocess.STDOUT, shell=True)
-		else:
-			cmd_output = subprocess.check_output("wget '{}'".format(url), stderr=subprocess.STDOUT, shell=True)
-		raw_filename = re.findall(r' - ‘(.*?)’ saved', cmd_output)
-		filename = str(raw_filename[0])
+	        else:
+               		cmd_output = subprocess.check_output("wget '{}'".format(url), stderr=subprocess.STDOUT, shell=True)
+                raw_filename = re.findall(r' - ‘(.*?)’ saved', cmd_output)
+                filename = str(raw_filename[0])
+        except Exception as e:
+                print(e)
+                ERROR = "ERROR CODE-a1"
+		return ERROR
+
+	return filename
