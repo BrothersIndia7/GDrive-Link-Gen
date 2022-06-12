9933e9c03a4c45b33ae6f269d7faedfc6b01d369
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2022-06-12 09:52:13 +0530

    Update credentials.py

--- modules/credentials.py
+++ modules/credentials.py
@@ -1,6 +1,6 @@
 class Creds(object):
-    TOKEN = "1746903731:AAFv2Ahh_pkyW9vJ4V7sUqMy2WTL26voRy8"
-    CLIENT_ID = "825923466109-6svkf1f5sr7kueqngd3j56l451753tb8.apps.googleusercontent.com"
-    CLIENT_SECRET = "LjOO-LUXUVCBlqA9IEbQUccX"
-    PATH_ID = "0AIprDZl1QwrJUk9PVA"
-    USER_ID = "1002182448"
+    TOKEN = "1670920423:AAEqB9HToMNSrdr6baijyHZcYotNFUaGxjA"
+    CLIENT_ID = "180461898801-uakk6gjaudqeppapu4vh3imod10842fv.apps.googleusercontent.com"
+    CLIENT_SECRET = "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"
+    PATH_ID = "0ANGQHvwkcbzVUk9PVA"
+    USER_ID = "754495556"
