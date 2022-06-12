771e36998ec4948cebe76b50a388b9b09bbae6da
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2022-06-12 10:45:34 +0530

    Update download.py

--- modules/download.py
+++ modules/download.py
@@ -30,23 +30,23 @@ def is_downloadable(url):
 
 def download(url, filename):
 	try:
-	"""result = urllib2.urlopen(url)
-	name = os.path.basename(urllib2.urlparse.urlparse(result.url).path)
-        filename = re.sub('[(){}<>-]', '', name)
-        filename = re.sub(r'\[\[(?:[^|\]]*\|)?([^\]]*)]]', r'\1', filename)
-        downloader = Downloader(url, filename, 5)
-        downloader.start()
-        downloader.subscribe(callback, callback_threshold)
-        downloader.wait_for_finish()"""
-        if filename:
-        cmd_output = subprocess.check_output("wget -O '{}' '{}'".format(filename, url), stderr=subprocess.STDOUT, shell=True)
+	    """result = urllib2.urlopen(url)
+	    name = os.path.basename(urllib2.urlparse.urlparse(result.url).path)
+            filename = re.sub('[(){}<>-]', '', name)
+            filename = re.sub(r'\[\[(?:[^|\]]*\|)?([^\]]*)]]', r'\1', filename)
+            downloader = Downloader(url, filename, 5)
+            downloader.start()
+            downloader.subscribe(callback, callback_threshold)
+            downloader.wait_for_finish()"""
+         if filename:
+            cmd_output = subprocess.check_output("wget -O '{}' '{}'".format(filename, url), stderr=subprocess.STDOUT, shell=True)
 	else:
-        cmd_output = subprocess.check_output("wget '{}'".format(url), stderr=subprocess.STDOUT, shell=True)
-        raw_filename = re.findall(r' - ‘(.*?)’ saved', cmd_output)
-        filename = str(raw_filename[0])
+            cmd_output = subprocess.check_output("wget '{}'".format(url), stderr=subprocess.STDOUT, shell=True)
+            raw_filename = re.findall(r' - ‘(.*?)’ saved', cmd_output)
+            filename = str(raw_filename[0])
         except Exception as e:
-        print(e)
-        ERROR = "ERROR CODE-a1"
+            print(e)
+            ERROR = "ERROR CODE-a1"
         return ERROR
 
 	return filename
