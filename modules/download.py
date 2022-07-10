671354622f8e97ec1b95ea00e9b8ec074912239e
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2022-07-10 18:12:41 +0530

    Update download.py

--- modules/download.py
+++ modules/download.py
@@ -44,7 +44,7 @@ def download(url, filename):
 			cmd_output = subprocess.check_output("wget '{}'".format(url), stderr=subprocess.STDOUT, shell=True)
 		raw_filename = re.findall(r' - ‘(.*?)’ saved', cmd_output)
 		filename = str(raw_filename[0])
-      	except Exception as e:
+         	except Exception as e:
 		print(e)
 		ERROR = "ERROR CODE-a1"
 		return ERROR
