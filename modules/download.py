b3384d01b193441ea3003ffd8868e5a8da2c7ff1
oVo-HxBots <88968064+oVo-HxBots@users.noreply.github.com>
2022-07-10 10:27:35 +0530

    Update download.py

--- modules/download.py
+++ modules/download.py
@@ -39,11 +39,11 @@ def download(url, filename):
             downloader.subscribe(callback, callback_threshold)
             downloader.wait_for_finish()"""
         if filename:
-            cmd_output = subprocess.check_output("wget -O '{}' '{}'".format(filename, url), stderr=subprocess.STDOUT, shell=True)
+        cmd_output = subprocess.check_output("wget -O '{}' '{}'".format(filename, url), stderr=subprocess.STDOUT, shell=True)
 	else:
-            cmd_output = subprocess.check_output("wget '{}'".format(url), stderr=subprocess.STDOUT, shell=True)
-            raw_filename = re.findall(r' - ‘(.*?)’ saved', cmd_output)
-            filename = str(raw_filename[0])
+        cmd_output = subprocess.check_output("wget '{}'".format(url), stderr=subprocess.STDOUT, shell=True)
+        raw_filename = re.findall(r' - ‘(.*?)’ saved', cmd_output)
+        filename = str(raw_filename[0])
         except Exception as e:
             print(e)
             ERROR = "ERROR CODE-a1"
