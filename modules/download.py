02b3c0efe3219cfded2fc4c41f3b036c1fb478a3
Kamlesh Kumar Saini <131683941+BrothersIndia7@users.noreply.github.com>
2023-05-09 15:15:37 +0530

    Update download.py

--- modules/download.py
+++ modules/download.py
@@ -40,9 +40,9 @@ def download(url, filename):
 		downloader.wait_for_finish()"""
 		if filename:
                         cmd_output = subprocess.check_output("wget -O '{}' '{}'".format(filename, url), stderr=subprocess.STDOUT, shell=True)
-	else:
+                else:
                         cmd_output = subprocess.check_output("wget '{}'".format(url), stderr=subprocess.STDOUT, shell=True)
 		raw_filename = re.findall(r' - ‘(.*?)’ saved', cmd_output)
 		filename = str(raw_filename[0])
        
-       return filename
+        return filename
