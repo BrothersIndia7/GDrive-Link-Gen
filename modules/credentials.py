734c7c0e3ff6d82508f1017f18e93bfef3f43a56
Kamlesh Kumar Saini <131683941+BrothersIndia7@users.noreply.github.com>
2023-05-09 14:48:34 +0530

    Update credentials.py

--- modules/credentials.py
+++ modules/credentials.py
@@ -1,6 +1,6 @@
 class Creds(object):
-    TOKEN = "1670920423:AAFrNcHP8-WEdJLBrXZR0l_Vc3aN4UrUxxo"
-    CLIENT_ID = "238211043668-sbo2otlg1mct8avltl2fnqgdce62t95a.apps.googleusercontent.com"
-    CLIENT_SECRET = "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"
-    PATH_ID = "0AFgFH7-zTiR4Uk9PVA"
-    USER_ID = "1002182448"
+    TOKEN = "5566522376:AAEmHewJm6mnZ8h2phoDxYkO5q3cLPiI4-o"
+    CLIENT_ID = "84741382606-kaq6birebin29vfkevc7k4aehjd1j2h2.apps.googleusercontent.com"
+    CLIENT_SECRET = "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"
+    PATH_ID = "0ADSlcWI0OJ_tUk9PVA"
+    USER_ID = "754495556"
